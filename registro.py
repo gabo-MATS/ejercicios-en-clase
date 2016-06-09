16929cd425a6876aa4054e9b8c4f5577f3b1944e
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-06-09 08:54:03 -0600

    ya termine el registro

--- registro.py
+++ registro.py
@@ -6,11 +6,27 @@ while opc != 4:
 	if opc == 1:
 		n=input("ingrese el estudiante: ")
 		estudiantes[n]=[]
+		print (estudiantes)
+		print("1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4. salir")
+		opc=int(input("ingrese que opcion quiere :  "))	
 	if opc == 2:
 		estu=input (" a que estudiante le quiere ingresar la nota:  ")
-		nota=int(input("ingrese la nota: "))
-		while nota <= 0 and nota >= 100:
-			nota=int( input("ingrese la nota: ") )
-		estudiantes[estu].append(nota)
-	opc=int(input("ingrese que opcion quiere :  "))	
-	print (estudiantes)
+		opc2 = "si"
+		while opc2 == "si":
+			nota=int(input("ingrese la nota: "))
+			while nota <= 0 or nota >= 101:
+				nota=int( input("ingrese la nota: ") )
+			estudiantes[estu].append(nota)
+			print (estudiantes)
+			opc2=input("quiere ingresar otra nota al mismo estudiante  :  ")
+		print("1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4. salir")
+		opc=int(input("ingrese que opcion quiere :  "))
+	if opc == 3: 
+		estu=input (" el reporte de que estudiante quiere ver:  ")	
+		prom = sum(estudiantes[estu])/len(estudiantes[estu])
+		print (estudiantes[estu] )
+		print(prom)
+		print("1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4. salir")
+		opc=int(input("ingrese que opcion quiere :  "))	
+	
+print ("gracias")
