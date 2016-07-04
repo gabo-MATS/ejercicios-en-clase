e8c401ecead845a5f37a614aed27100d205fea09
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-07-04 08:58:39 -0600

    prueba totito

---
+++ totito/funtotito.py
@@ -0,0 +1,7 @@
+def marca (m ,l):
+	for i in range (len(l)):
+		if m == l[i]:
+			l[i]=X
+
+	return l		
+
---
+++ totito/totito.py
@@ -0,0 +1,8 @@
+from funtotito import marca
+l= [[a,b,c]
+	[d,e,f]
+	[g,h,i]
+	]
+print (l) 
+m=input ("ingrese pocision")
+print (l	)
