221b510add73791a6602402ef1351253c4feb6b1
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-07-11 08:42:21 -0600

    totito terminado

--- totito/totito.py
+++ totito/totito.py
@@ -1,4 +1,3 @@
-from funtotito import gano
 print ("bienvenidos")
 l= [["a","b","c"],
 	["d","e","f"],
@@ -9,7 +8,7 @@ while w!=True:
 	print (l[0])
 	print (l[1])
 	print (l[2]) 
-	s=str(input("ingrese pocision: "))
+	s=str(input("ingrese pocision jugador 1 : "))
 	if s=="a":
 		l[0][0]="X"
 	if s=="b":
@@ -28,30 +27,77 @@ while w!=True:
 		l[2][1]="X"	
 	if s=="i":
 		l[2][2]="X"
-	if l[0][0]and l[0][1]and l[0][2]=="X":
+	if l[0][0]=="X" and l[0][1]=="X" and l[0][2]=="X":
 		print ("jugador 1 gano") 	
 		w=True
-	if l[1][0]and l[1][1]and l[1][2]=="X":
+	if l[1][0]=="X" and l[1][1]=="X" and l[1][2]=="X":
 		print ("jugador 1 gano") 	
 		w=True	
-	if l[2][0]and l[2][1]and l[2][2]=="X":
+	if l[2][0]=="X" and l[2][1]=="X" and l[2][2]=="X":
 		print ("jugador 1 gano") 	
 		w=True
-	if l[0][0]and l[1][0]and l[2][0]=="X":
+	if l[0][0]=="X" and l[1][0]=="X" and l[2][0]=="X":
 		print ("jugador 1 gano") 	
 		w=True		
-	if l[0][1]and l[1][1]and l[2][1]=="X":
+	if l[0][1]=="X" and l[1][1]=="X" and l[2][1]=="X":
 		print ("jugador 1 gano") 	
 		w=True
-	if l[0][2]and l[1][2]and l[2][2]=="X":
+	if l[0][2]=="X" and l[1][2]=="X" and l[2][2]=="X":
 		print ("jugador 1 gano") 	
 		w=True	
-	if l[0][0]and l[1][1]and l[2][2]=="X":
+	if l[0][0]=="X" and l[1][1]=="X" and l[2][2]=="X":
 		print ("jugador 1 gano") 	
 		w=True
-	if l[0][2]and l[1][1]and l[2][0]=="X":
+	if l[0][2]=="X" and l[1][1]=="X" and l[2][0]=="X":
 		print ("jugador 1 gano") 	
-		w=True		
+		w=True	
+	if w != True:	
+		print (l[0])
+		print (l[1])
+		print (l[2]) 
+		s=str(input("ingrese pocision jugador 2 : "))
+		if s=="a":
+			l[0][0]="O"
+		if s=="b":
+			l[0][1]="O"
+		if s=="c":
+			l[0][2]="O"
+		if s=="d":
+			l[1][0]="O"
+		if s=="e":
+			l[1][1]="O"
+		if s=="f":
+			l[1][2]="O"
+		if s=="g":
+			l[2][0]="O"
+		if s=="h":
+			l[2][1]="O"	
+		if s=="i":
+			l[2][2]="O"
+		if l[0][0]=="O" and l[0][1]=="O" and l[0][2]=="O":
+			print ("jugador 2 gano") 	
+			w=True
+		if l[1][0]=="O" and l[1][1]=="O" and l[1][2]=="O":
+			print ("jugador 2 gano") 	
+			w=True	
+		if l[2][0]=="O" and l[2][1]=="O" and l[2][2]=="O":
+			print ("jugador 2 gano") 	
+			w=True
+		if l[0][0]=="O" and l[1][0]=="O" and l[2][0]=="O":
+			print ("jugador 2 gano") 	
+			w=True		
+		if l[0][1]=="O" and l[1][1]=="O" and l[2][1]=="O":
+			print ("jugador 2 gano") 	
+			w=True
+		if l[0][2]=="O" and l[1][2]=="O" and l[2][2]=="O":
+			print ("jugador 2 gano") 	
+			w=True	
+		if l[0][0]=="O" and l[1][1]=="O" and l[2][2]=="O":
+			print ("jugador 2 gano") 	
+			w=True
+		if l[0][2]=="O" and l[1][1]=="O" and l[2][0]=="O":
+			print ("jugador 2 gano") 	
+			w=True				
 print (l[0])
 print (l[1])
 print (l[2]) 
