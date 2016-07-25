688fcc41fd6b153629e79fd733bd4c7f77ce7cd9
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-07-25 09:01:05 -0600

    ahorcado lunes

--- ahorcado/ahorcado.py
+++ ahorcado/ahorcado.py
@@ -1,19 +1,23 @@
 from palabras import palabra
 from gano import gano 
-
+from muñeco import mu
 print ("bienvenidos")
 j="si"
 gano=False
 v=" "
+p=palabra()
 while j == "si" :
 	w=palabra()
 	sumaw=len(w)
-	pal=list(palabra)
+	pal=list(p)
 	while gano != "si" and gano != "no" :
 		print ("_ "*sumaw)
 		l=input ("ingrse una letra: ")
-		gano=gano(res)
+		gano=gano(l,pal)
 		print (v)
+		if gano == False:
+			print (mu)
+
 
 	j=input("desea seguir jugando: ")	
 
--- ahorcado/gano.py
+++ ahorcado/gano.py
@@ -1,5 +1,9 @@
 def gano (res,l,pal):
 	if l in pal :
 		v=" "
+		gano = True
 		return gano
 		return v
+	if l not in pal:
+		gano = False
+		return gano
