2a2d02f220d615e50c34c0672796c05e32b2caf3
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-07-27 09:00:20 -0600

    ahorcado terminado

--- ahorcado/ahorcado.py
+++ ahorcado/ahorcado.py
@@ -3,22 +3,34 @@ from gano import ga
 from muñeco import mu
 print ("bienvenidos")
 j="si"
-gano=False
 v=0
 while j == "si" :
 	w=palabra()
-	sumaw=len(w)
 	pal=list(w)
+	guiones = []
+	gano=False
+	v=0
 	print (w)
+	for l in w:
+			guiones.append(" _")
 	while gano != "si" and gano != "no" :
-		print ("_ "*sumaw)
+		print ("".join(guiones))	
 		l=input ("ingrse una letra: ")
+		for i in range (len(w)):
+			if pal[i]==l:
+				guiones[i]=l
 		gano=ga(l,pal)
 		if gano == False:
 			v=v+1
-			print (mu(v)) 
-				
+			print (mu(v))
+			if v == 7:
+				gano = "no"
+		if " _"	not in guiones:
+			gano ="si"	 		
 
-
+	if gano == "no":
+		print ("perdio") 
+	if gano == "si":
+		print ("gano!!!!!") 				
 	j=input("desea seguir jugando: ")	
-
+print ("gracias por jugar")
