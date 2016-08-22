02ab37fa394f3036bb6072becf96484d484e3cdf
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-08-22 08:58:55 -0600

    instrucciones

--- robot/rulplay.py
+++ robot/rulplay.py
@@ -1,11 +1,26 @@
+mapa_final = []
 def cargar_mapa(mapa):
-	mapa_final = []
+	
 	f = open(mapa, "r")
 	
 	for line in f:
 		mapa_final.append(list(line.strip()))
-
+		
 	return mapa_final
 
 mi_mapa = cargar_mapa("mapas/mapa1.txt")
 print (mapa_final)
+
+ordenes = []
+def cargar_instrucciones(instrucciones):
+	
+	h = open(instrucciones, "r")
+
+	for regla in h:
+		ordenes.append(list(regla.split()))
+
+	return ordenes
+
+reglas = cargar_instrucciones("instrucciones/programa1.txt")	
+print (ordenes)
+
