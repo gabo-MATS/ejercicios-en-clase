d94f2ab78b6d443bb843b2a6ebafcfdd5429a4fe
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-08-25 08:26:47 -0600

    prueba

--- robot/robot.py
+++ robot/robot.py
@@ -1,3 +1,23 @@
 class Robot(object):
-	def __init__(self,girar,caminar,levantar):
-		
+	def __init__(self,direccion,x,y,fichas,mapa):
+		self.direccion=0
+		self.x=0
+		self.y=0
+		self.fichas=0
+		self.mapa= mapa_final
+	def caminar(self):
+		if self.direccion==0 and self.x<80:
+			self.x+=1	
+		if self.direccion==90 and self.y<25:
+			self.y+=1
+		if self.direccion==180 and self.x>0:
+			self.x-=1
+		if self.direccion==270 and self.y>0:
+			self.y-=1			
+	def rotar(self):
+		self.direccion-=90
+		if self.direccion <0:
+			self.direccion=270
+	def recoger(self):
+		self.fichas+=1					
+#^=90 <=180 >= 0  v=270
