48634112590a2fcc9ea87a391cff02120e606c56
gabo-MATS <gabrielmartinez1222@gmail.com>
2016-09-19 09:03:31 -0600

    fgj

--- ejercicios-en-clase-master/robot/utilidades.py
+++ ejercicios-en-clase-master/robot/utilidades.py
@@ -1,13 +1,13 @@
-mapa_final = []
 def cargar_mapa(mapa):	
+	mapa_final = []
 	f = open(mapa, "r")	
 	for line in f:
 		mapa_final.append(list(line.strip()))
 	return mapa_final
 
 
-ordenes = []
 def cargar_instrucciones(instrucciones):	
+	ordenes = []
 	h = open(instrucciones, "r")
 	for regla in h:
 		ordenes.append(regla.strip())
