8dd4e66a73a5d1130cb05e5a52728287dc451f8d
gabo-MATS <gabrielmartinez1222@gmail.com>
2017-02-02 11:55:08 -0600

    request

--- 5to bach/tab.py
+++ 5to bach/tab.py
@@ -1,10 +1,13 @@
+import request
 class Tab (object)
 	def __init__(self,URL,nombre):
 		self.URL=URL
 		self.nombre=nombre
 
 
-	def descargar_URL(self):
+	def descargar_URL(self,URL):
+		r = request.get(URL)
+		
 		
 	
 
